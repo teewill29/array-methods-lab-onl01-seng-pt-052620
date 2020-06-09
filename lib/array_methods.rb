704b61103db5644bbe59be0975d5449e39256742
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 04:05:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,7 @@
-def using_include(array, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
+
+def using_include(array, element)
+    array = array
     element1 = "wow"
     element2 = "Tardis"
     array.include?(element1)
