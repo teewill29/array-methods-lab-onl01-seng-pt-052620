0d03df7e6c630957ca11cc2f52dca6bc871965f6
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 04:10:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,10 @@ array = ["wow", "I", "am", "really", "learning", "arrays!"]
 def using_include(array, element)
     element1 = "wow"
     element2 = "Tardis"
-    array.include?(element1)
-    array.include?(element2)
+    array.include?
+    (element1)
+    array.include?
+    (element2)
 end
 
 def using_sort(array)
